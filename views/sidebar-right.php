043a6d1b5a4d00505fe2beae7c1a5247c8c5e5a2
Max Kostinevich <max.kostinevich@gmail.com>
2014-10-01 09:13:59 +0300

    Fix sidebar

--- views/sidebar-right.php
+++ views/sidebar-right.php
@@ -1,7 +1,7 @@
 			<div id="postbox-container-1" class="postbox-container cw-wpemaw-right-sidebar-admin">
 				<div class="meta-box-sortables">
 					<div class="postbox">
-						<h3><span><?php _e('Help Center',$cw_wpaom_plugin_slug);?></span></h3>
+						<h3><span><?php _e('Help Center');?></span></h3>
 						<div class="inside">
 							<div><a href="http://codecanyon.net/user/circlewaves/portfolio?ref=circlewaves" target="_blank" title=""><img src="<?php echo plugins_url( 'assets/img/btn_follow.png',dirname(__FILE__));?>" /></a></div>
 							<div><a href="http://extras.envato.com/api/" target="_blank" title=""><img src="<?php echo plugins_url( 'assets/img/envato_api_logo.png',dirname(__FILE__));?>" /></a></div>
